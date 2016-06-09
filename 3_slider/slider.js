8b95a58f38fa2e144e5f29987dabf32ee08f8df9
leeYoonGu <lee36656@gmail.com>
2016-06-09 16:47:31 +0900

    dot 클릭 이벤트 추가

--- 3_slider/slider.js
+++ 3_slider/slider.js
@@ -1,8 +1,10 @@
-
 var ul = document.getElementById('slider'); // 배열로 가져올 시 style.width가 먹지 않아 id 지정 
 var li = ul.children; // li 들을 children을 통해 배열로 저장한다. 
 var liWidth = ul.children[0].offsetWidth; // li 의 width값을 가져옴 
 var i = 0;
+
+document.getElementById("btn_next").addEventListener("click", moveNext);
+document.getElementById("btn_prev").addEventListener("click", movePrev);
 
 function moveNext() {
   if (i < li.length - 1) { //li 갯수만큼만 slider 실행
@@ -18,5 +20,12 @@ function movePrev() {
   } 
 }                
 
-document.getElementById("btn_next").addEventListener("click", moveNext);
-document.getElementById("btn_prev").addEventListener("click", movePrev);
+var dot=document.getElementById("dot");
+var dot_a=dot.getElementsByTagName("a");
+
+for(var k=0; k<dot_a.length -1; k++){ 
+  dot_a[k].addEventListener("click", movePoint); // 모든 dot에 이벤트 걸기
+  function movePoint() { 
+    ul.style.marginLeft = -liWidth * k + "px";     
+  }
+}
